1039d8d4f9140da7b0d7761f3d8e5b4f7ad64e64
Chiho <87627359+usernamechiho@users.noreply.github.com>
2025-02-25 21:22:30 +0900

    fix: Supabase type generator 추가(#7)

--- packages/supabase/src/types/index.ts
+++ packages/supabase/src/types/index.ts
@@ -1,2 +1,9 @@
 /** 추후 Supabase의 OAuth 기능 추가시 사용  */
 export { type Provider } from '@supabase/supabase-js';
+
+import { Database } from './supabase';
+
+/** supabase.ts 사용 예제 */
+type TodoInfo = Database['public']['Tables']['todo-temp']['Row'];
+
+export type { TodoInfo };
